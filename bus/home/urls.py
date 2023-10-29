e69a5f181372c36ce670c366f2923bc41723646c
vikas singh <vksr90244@example.com>
2023-10-29 20:55:24 +0530

    update code

--- bus/home/urls.py
+++ bus/home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("save_customer/", views.customer, name="customer")
+    path("save_customer/", views.customer, name="customer"),
+    path("about/", views.about, name ="about")
 ]
--- bus/home/views.py
+++ bus/home/views.py
@@ -5,6 +5,8 @@ from django.db import connection
 
 def home(request):
     return render(request, "index.html")
+def about(request):
+    return render(request, "about.html")
 def customer(request):
    if request.method =='POST' :
         try :
